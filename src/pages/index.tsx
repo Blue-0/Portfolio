b7170fa5ddfbc09fedd094dc118672e5efd81ba1
blue <denis.flt@icloud.com>
2024-03-16 00:29:43 +0100

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Head from "next/head";
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+
+      <Seo title="Denis Fluteaux Portfolio" description="Mon Porfolio trop classe" />
+      <h1> Hello World 485641653</h1>
+
+    </>
+    );
+}
